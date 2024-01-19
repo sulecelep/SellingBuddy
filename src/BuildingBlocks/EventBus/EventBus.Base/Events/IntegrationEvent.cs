1725e603fb40e3679ecf28ae435b6096e121e143
ssule <ssulecelepp@gmail.com>
2024-01-19 17:12:05 +0300

    Add project files.

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Events/IntegrationEvent.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Base.Events
+{
+    public class IntegrationEvent
+    {
+        //bu event ne zaman oluşturuldu ve Id'si
+        [JsonProperty]
+        public Guid Id { get; private set; }
+        [JsonProperty]
+        public DateTime CreatedDate { get; private set; }
+        
+        public IntegrationEvent()
+        {
+            Id = Guid.NewGuid();
+            CreatedDate = DateTime.Now;
+        }
+        [JsonConstructor]
+        public IntegrationEvent(Guid id, DateTime createdDate)
+        {
+            Id = id;
+            CreatedDate = createdDate;
+        }
+        
+    }
+}
---
+++ src/BuildingBlocks/EventBus/EventBus.Base/SubscriptionInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Base
+{
+    public class SubscriptionInfo
+    {
+        //dışarıdan bize gönderilen verilerin tipini döndürür
+        public Type HandlerType { get; }
+
+        public SubscriptionInfo(Type handlerType)
+        {
+            HandlerType = handlerType ?? throw new ArgumentNullException(nameof(handlerType));
+        }
+        public static SubscriptionInfo Typed(Type handlerType)
+        {
+            return new SubscriptionInfo(handlerType);
+        }
+    }
+}
